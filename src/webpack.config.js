d02ced23fcb8e3573f254cffb518d6cf1ceec145
AnnaKoziol1997 <85227390+AnnaKoziol1997@users.noreply.github.com>
2022-09-19 23:08:18 +0200

    webpack cd

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,46 +1,63 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const deps = require("./package.json").dependencies;
 
 module.exports = {
-  mode: 'development',
-  devServer: {
-    port: 8083,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
 
-        exclude: /node_modules/,
+    output: {
+        publicPath: "https://mfremote-e872c.firebaseapp.com/",
+    },
 
-        loader:
-          'babel-loader',
-        options: {
-          presets: [
-            '@babel/preset-env',
-            '@babel/preset-react',
-          ],
-        },
-      },
+    mode: 'development',
+    devServer: {
+        port: 8083,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+
+                exclude: /node_modules/,
+
+                loader:
+                    'babel-loader',
+                options: {
+                    presets: [
+                        '@babel/preset-env',
+                        '@babel/preset-react',
+                    ],
+                },
+            },
+        ],
+    },
+
+    plugins: [
+        new ModuleFederationPlugin(
+            {
+                name: 'REMOTE',
+                filename:
+                    'remoteEntry.js',
+
+                exposes: {
+                    './App':
+                        './src/App',
+                },
+                shared: {
+                    ...deps,
+                    react: {
+                        singleton: true,
+                        requiredVersion: deps.react,
+                    },
+                    "react-dom": {
+                        singleton: true,
+                        requiredVersion: deps["react-dom"],
+                    },
+                },
+            }
+        ),
+        new HtmlWebpackPlugin({
+            template:
+                './public/index.html',
+        }),
     ],
-  },
-
-  plugins: [
-    new ModuleFederationPlugin(
-      {
-        name: 'REMOTE',
-        filename:
-          'remoteEntry.js',
-
-        exposes: {
-          './App':
-            './src/App',
-        },
-      }
-    ),
-    new HtmlWebpackPlugin({
-      template:
-        './public/index.html',
-    }),
-  ],
 };
